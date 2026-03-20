6aa4b8fe1e54a0c5f316c8ba24932d394db7e82b
Gaurav1922 <gauravmanjunath25@gmail.com>
2026-03-20 13:23:31 +0530

    Update urls.py

--- backend/sports_booking/accounts/urls.py
+++ backend/sports_booking/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import create_admin
+from .views import create_admin, reset_admin
 from django.http import JsonResponse
 from django.utils import timezone
 
